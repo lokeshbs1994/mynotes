2ae1c3ef37fb726bc1a6d32e49621c2f79074fd8
lokeshbs1994 <lokeshbs619@gmail.com>
2022-06-18 08:27:18 +0530

    step-2

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -66,7 +66,7 @@ class _LoginViewState extends State<LoginView> {
                 } on FirebaseAuthException catch (e) {
                   if (e.code == 'user-not-found') {
                     print('User not found');
-                  } else if (e.code == 'wrong_password') {
+                  } else if (e.code == 'wrong-password') {
                     print('Wrong password');
                   }
                 }
