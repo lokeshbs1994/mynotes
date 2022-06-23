be1da8faf9384a2c40df575f5c0d0654fc94f03a
lokeshbs1994 <lokeshbs619@gmail.com>
2022-06-23 20:38:43 +0530

    step-24

--- lib/services/auth/bloc/auth_event.dart
+++ lib/services/auth/bloc/auth_event.dart
@@ -15,6 +15,20 @@ class AuthEventLogIn extends AuthEvent {
   const AuthEventLogIn(this.email, this.password);
 }
 
+class AuthEventSendEmailVerification extends AuthEvent {
+  const AuthEventSendEmailVerification();
+}
+
+class AuthEventRegister extends AuthEvent {
+  final String email;
+  final String password;
+  const AuthEventRegister(this.email, this.password);
+}
+
+class AuthEventShouldRegister extends AuthEvent {
+  const AuthEventShouldRegister();
+}
+
 class AuthEventLogOut extends AuthEvent {
   const AuthEventLogOut();
 }
